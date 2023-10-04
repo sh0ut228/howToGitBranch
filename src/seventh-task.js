ad532b446c6c2caeb2b562ecd3676ea593a7b4af
sh0ut228 <pains_97@mail.ru>
2023-10-04 14:09:28 +0300

    seventh-task

---
+++ src/seventh-task.js
@@ -0,0 +1,7 @@
+const Ploschad=70;
+const stora=7;
+const storb=3;
+const storc=9;
+const polper=(stora+storb+storc)/2;
+
+console.log(Math.sqrt(polper* (polper - stora) * (polper - storb) * (polper - storc)));
